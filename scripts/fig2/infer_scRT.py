2e4251ef565f7743a173b6533c0781034683744c
Adam Weiner <adamcweiner@gmail.com>
2022-03-02 13:57:08 -0500

    fix typo when printing shapes

--- scripts/fig2/infer_scRT.py
+++ scripts/fig2/infer_scRT.py
@@ -1,7 +1,6 @@
 from argparse import ArgumentParser
 import numpy as np
 import pandas as pd
-import logging
 from scdna_replication_tools.infer_scRT import scRT
 
 
@@ -20,7 +19,7 @@ def main():
     argv = get_args()
     cn_s = pd.read_csv(argv.cn_s, sep='\t')
     cn_g1 = pd.read_csv(argv.cn_g1, sep='\t')
-    logging.info('loaded data')
+    print('loaded data')
 
     # make sure clone_id column is present if none are listed
     if 'clone_id' not in cn_g1.columns:
@@ -35,21 +34,21 @@ def main():
     temp_cn_s = cn_s[['cell_id', 'chr', 'start', 'end', 'gc', 'state', argv.input_col]]
     temp_cn_g1 = cn_g1[['cell_id', 'chr', 'start', 'end', 'gc', 'clone_id', 'state', argv.input_col]]
 
-    logging.info('creating scrt object')
+    print('creating scrt object')
     # create SPF object with input
     scrt = scRT(temp_cn_s, temp_cn_g1, input_col=argv.input_col, clone_col='clone_id')
 
-    logging.info('running inference')
+    print('running inference')
     # run inference
     cn_s_with_scrt = scrt.infer(level='bulk')
-    logging.info('done running inference')
+    print('done running inference')
 
-    logging.info('cn_s.shape {}'.format(cn_s.shape))
-    logging.info('cn_s_with_clone_id.shape {}'.format(cn_s_with_clone_id.shape))
+    print('cn_s.shape {}'.format(cn_s.shape))
+    print('cn_s_with_scrt.shape {}'.format(cn_s_with_scrt.shape))
 
-    # merge cn_s_with_clone_id with initial cn_s input to add columns that were excluded from temp_cn_s
+    # merge cn_s_with_scrt with initial cn_s input to add columns that were excluded from temp_cn_s
     cn_s_out = pd.merge(cn_s, cn_s_with_scrt)
-    logging.info('cn_s_out.shape {}'.format(cn_s_out.shape))
+    print('cn_s_out.shape {}'.format(cn_s_out.shape))
 
     # save output files
     cn_s_out.to_csv(argv.cn_s_out, sep='\t', index=False)
