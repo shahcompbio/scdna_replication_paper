c7ff5d838bddec2461b8c6c0ccc09ea8a336a46c
Adam Weiner <adamcweiner@gmail.com>
2022-03-11 16:30:24 -0500

    bump

--- scripts/fig2/simulate_diploid_data.py
+++ scripts/fig2/simulate_diploid_data.py
@@ -1,8 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import scale
-from sklearn.linear_model import LinearRegression
-from scipy.optimize import curve_fit
 from argparse import ArgumentParser
 
 def get_args():
