26384d70a4ae4c77ca8c4c9213f833b93d0dc42f
Adam Weiner <adamcweiner@gmail.com>
2023-07-06 14:10:55 -0400

    use multivariate linear regression for PCA

--- scripts/rt_model/clone_pca_regression.py
+++ scripts/rt_model/clone_pca_regression.py
@@ -33,28 +33,31 @@ def main():
         'signature_HRD', 'signature_TD', 'signature_FBI'
     ]
 
+    target_cols = [
+        'PC1', 'PC2', 'PC3', 'PC4', 'PC5'
+    ]
+
     cv = RepeatedKFold(n_splits=5, n_repeats=5, random_state=0)
 
     # transform the features to have mean 0 and variance 1
     X = StandardScaler().fit_transform(clone_embeddings[feature_cols])
-    alphas = np.logspace(-10, 10, 21)
+    y = clone_embeddings[target_cols]
+
+    # use the features to predict the PC on each fold
+    cv_model = cross_validate(
+        LinearRegression(),
+        X,
+        y,
+        cv=cv,
+        return_estimator=True,
+        n_jobs=2,
+    )
+
+    # add the coefficients to the dataframe
     feature_coef_df =[]
-    for i in range(5):
-        # get the ith PC
-        ycol = 'PC{}'.format(i+1)
-        y = clone_embeddings[ycol]
-        # use the features to predict the PC on each fold
-        cv_model = cross_validate(
-            LinearRegression(),
-            X,
-            y,
-            cv=cv,
-            return_estimator=True,
-            n_jobs=2,
-        )
-        # add the coefficients to the dataframe
-        for est in cv_model["estimator"]:
-            temp_df = pd.DataFrame({'feature': feature_cols, 'coef': est.coef_})
+    for est in cv_model["estimator"]:
+        for i, ycol in enumerate(target_cols):
+            temp_df = pd.DataFrame({'feature': feature_cols, 'coef': est.coef_[i]})
             temp_df['PC'] = ycol
             feature_coef_df.append(temp_df)
 
@@ -62,15 +65,35 @@ def main():
 
     # plot the log10 of absolute value of the coefficients
     feature_coef_df['log10_abs_coef'] = np.log10(abs(feature_coef_df['coef']))
+
+    # remove rows from feature_coef_df where coef==0 as these represent cases where the cross-validation resulted in a model that did not use that feature
+    feature_coef_df = feature_coef_df.query('coef!=0')
+
+    # create a new column named 'metafeature' which merges all the one-hot encoded features into a single feature
+    feature_coef_df['metafeature'] = feature_coef_df['feature'].apply(lambda x: x.split('_')[0])
     
-    fig = plt.figure(figsize=(12, 4))
-    sns.barplot(data=feature_coef_df, x='feature', y='log10_abs_coef', hue='PC')
-    plt.ylabel('log10(abs(Coefficient))')
-    plt.xlabel('Feature')
-    plt.title('K-fold CV of linear regression coefficients for each PC')
+    fig, ax = plt.subplots(1, 4, figsize=(16, 4))
+    # merge the first three subplots into one supblot
+    ax[0].remove()
+    ax[1].remove()
+    ax[2].remove()
+    # create a new subplot in the first position that spans the first three subplots
+    ax[0] = fig.add_subplot(1, 4, (1, 3))
+
+    sns.barplot(data=feature_coef_df, x='feature', y='log10_abs_coef', hue='PC', ax=ax[0])
+    ax[0].set_ylabel('log10(abs(Coefficient))')
+    ax[0].set_xlabel('Feature')
+    ax[0].set_title('Multivariate linear regression of PCs\nusing K-fold CV')
     # reset the x-tick labels to rename '_' to '\n'
-    plt.xticks(range(len(feature_cols)), [x.replace('_', '\n') for x in feature_cols])
-    fig.savefig(argv.plot, dpi=300, bbox_inches='tight')
+    ax[0].set_xticklabels([x.replace('_', '\n') for x in feature_cols])
+
+    sns.barplot(data=feature_coef_df, x='metafeature', y='log10_abs_coef', hue='PC', ax=ax[3])
+    ax[3].set_ylabel('log10(abs(Coefficient))')
+    ax[3].set_xlabel('Feature')
+    ax[3].set_title('Multivariate linear regression of PCs\nusing K-fold CV')
+
+    # save the figure
+    fig.savefig(argv.plot, bbox_inches='tight', dpi=300)
 
     # save the coefficients to a table
     feature_coef_df.to_csv(argv.coefficients, index=False)
